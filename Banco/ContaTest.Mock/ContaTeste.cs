0484ab1cf031c4b5a659fa80befa6f2ffa4d42eb
Wodson Correia <wodson.correia@bemobi.com.br>
2020-10-01 16:26:15 -0300

    commit dev

--- Banco/ContaTest.Mock/ContaTeste.cs
+++ Banco/ContaTest.Mock/ContaTeste.cs
@@ -1,22 +1,38 @@
 using Conta;
+using Moq;
 using NUnit.Framework;
 
 namespace ContaTest.Mock
 {
     public class Tests
     {
-        private ContaBancaria _conta;
 
         [SetUp]
         public void Setup()
         {
-            _conta = new ContaBancaria("23069551823", 200);
+
         }
 
         [Test]
         public void TestSolicitarEmprestimo()
         {
-            
+            var conta = new ContaBancaria("23069551823", 100);
+
+            //implementa uma classe fake
+            var mock = new Mock<IValidadorCredito>();
+
+            //basicamente efetua a chamada do metodo e estabalece o retorno como [true]
+            mock.Setup(x => x.ValidarCredito("23069551823", 5000)).Returns(true);
+
+            //instancia um mock da sua interface
+            conta.SetValidadorCredito(mock.Object);
+
+            var resultadoEsperado = 5100;
+            conta.SolicitarEmprestimo(5000);
+
+            Assert.IsTrue(conta.GetSaldo() == resultadoEsperado);
+
+
         }
     }
 }
